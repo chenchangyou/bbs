0bb68923dead2b1cc52142d057c85e366f9a8b5f
ccy <1554693372@qq.com>
2019-06-15 00:52:11 +0800

    帖子点赞功能已完善，下一步预计更新评论区的点赞功能

--- src/main/java/com/youren/bbs/entity/Post.java
+++ src/main/java/com/youren/bbs/entity/Post.java
@@ -3,6 +3,7 @@ package com.youren.bbs.entity;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * 帖子
@@ -15,10 +16,11 @@ public class Post {
     private Date createTime;//发贴时间
     private User user;//发帖人
     private Integer awesome; //点赞量
+//    private List<Fabulous> fabulousList;//点赞量
     private Integer replynumber;//回复数量
     private Integer browse; //浏览数
     private Integer difference;//差评数
     private Integer state;//帖子状态（1.正常，2.正在审核，3.提示不合规范下架，并回退到草稿区，4，属于草稿
     private Category category;//帖子类别
-    private Fabulous fabulous;//用于判断当前用户是否点赞过帖子
+//    private Fabulous fabulous;//用于判断当前用户是否点赞过帖子
 }
